b8e4f2306b01ad624e2a6ebd299ef818a411a305
Taha Jalil <taha.jalil@gmail.com>
2014-03-26 16:19:45 -0400

    improved readability of comment, exercise complete

--- exercise5.rb
+++ exercise5.rb
@@ -2,7 +2,10 @@ puts "Please enter a temperature in Fahrenheit: "
 fahrenheit_temp = gets.chomp.to_f
 
 def fahrenheit_to_celsius(fahrenheit_temp)
-  celsius_temp = ((fahrenheit_temp - 32) * (5.0/9.0)) #breaks if we put a bracket around 5/9. This is because 5 and 9 are integers, and 5/9 is not. Gives 0. Solution: put either 5 as 5.0 or 9 as 9.0 OR run .to_f on either one of them, or both.
+  celsius_temp = ((fahrenheit_temp - 32) * (5.0/9.0)) 
+  #breaks if we put a bracket around 5/9. This is because 5 and 9 are integers, and 5/9 is not. Gives 0. 
+  #Solution: put either 5 as 5.0 or 9 as 9.0 OR run .to_f on either one of them, or both.
+  
   return "%0.1f" % celsius_temp
 end
 
