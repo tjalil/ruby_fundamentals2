e8b593f729f6d879fcbe00a7a1f8741b0fcb34e1
Taha Jalil <taha.jalil@gmail.com>
2014-03-26 16:15:50 -0400

    converted temperature to float

--- exercise5.rb
+++ exercise5.rb
@@ -1,8 +1,9 @@
 puts "Please enter a temperature in Fahrenheit: "
-fahrenheit_temp = gets.chomp.to_i
+fahrenheit_temp = gets.chomp.to_f
 
 def fahrenheit_to_celsius(fahrenheit_temp)
-  return ((fahrenheit_temp - 32) * 5/9)
+  celsius_temp = ((fahrenheit_temp - 32) * (5.0/9.0)) #breaks if we put a bracket around 5/9. This is because 5 and 9 are integers, and 5/9 is not. Gives 0. Solution: put either 5 as 5.0 or 9 as 9.0 OR run .to_f on either one of them, or both.
+  return "%0.1f" % celsius_temp
 end
 
 puts "A temperature of #{fahrenheit_temp}F is equal to #{fahrenheit_to_celsius(fahrenheit_temp)}C"
