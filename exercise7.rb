41f499b35b90aa2fb93c3431bb271ab5024dcba8
Taha Jalil <taha.jalil@gmail.com>
2014-03-26 17:35:00 -0400

    created cohort_stats method to display cohort names and # of students

--- exercise7.rb
+++ exercise7.rb
@@ -3,3 +3,11 @@ students = {
   cohort2: 42,
   cohort3: 22
 }
+
+def cohort_stats(students)
+  students.each do |key, value|
+    puts "#{key}: #{value} students"
+  end
+end
+
+cohort_stats(students)
