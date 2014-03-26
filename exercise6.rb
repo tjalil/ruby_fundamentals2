5300b2ef3c48f9468d0b034a3516d5b0aae9b7c8
Taha Jalil <taha.jalil@gmail.com>
2014-03-26 16:47:57 -0400

    added rice to list and listed each itme again

--- exercise6.rb
+++ exercise6.rb
@@ -1,5 +1,16 @@
+#grocery_list array
 grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
 
+#listing each item in grocery_list with * before it
 grocery_list.each do |item|
   puts "* #{item}"
 end
+
+#add rice to the list
+grocery_list << "rice"
+
+#listing each item in grocery_list with * before it
+grocery_list.each do |item|
+  puts "* #{item}"
+end
+
