d7c035c97ac8788b4abe3e625be8d44a01570268
Taha Jalil <taha.jalil@gmail.com>
2014-03-26 16:55:14 -0400

    added method 'list_items' to list items

--- exercise6.rb
+++ exercise6.rb
@@ -1,16 +1,26 @@
 #grocery_list array
 grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
 
-#listing each item in grocery_list with * before it
-grocery_list.each do |item|
-  puts "* #{item}"
+def list_items(grocery_list)
+  grocery_list.each do |item|
+    puts "* #{item}"
+  end
 end
+
+list_items(grocery_list)
+
+#listing each item in grocery_list with * before it
+#grocery_list.each do |item|
+# puts "* #{item}"
+#end
 
 #add rice to the list
 grocery_list << "rice"
 
 #listing each item in grocery_list with * before it
-grocery_list.each do |item|
-  puts "* #{item}"
-end
+#grocery_list.each do |item|
+# puts "* #{item}"
+#end
+
+list_items(grocery_list)
 
